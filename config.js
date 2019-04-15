677027574000990249d9cbdc4d442dc772f5c941
eonae <eonae.white@gmail.com>
2019-04-15 14:59:12 +0300

    ...

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   port: 8081,
-  mongoUri: 'mongodb://localhost:27017/todo'
+  mongoUri: 'mongodb://heroku_b7qdcckm:ujpGhGZ6@ds127376.mlab.com:27376/heroku_b7qdcckm'
+  // mongoUri: 'mongodb://localhost:27017/todo'
 }
